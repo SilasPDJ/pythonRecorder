464881438b7eeb1c12aec0d4b9b8ff60905e56eb
SilasPDJ <silasbertholdoferreira@gmail.com>
2021-07-25 14:32:17 -0300

    menubar update

--- os_tres_juntos.py
+++ os_tres_juntos.py
@@ -66,14 +66,37 @@ class MainApplication(Backend, AppInit):
     # Elements and placements
 
     def menubar(self):
+        def donothing():
+            pass
         menubar = tk.Menu(self.parent)
 
-        filemenu = tk.Menu(menubar)
-        filemenu.add_command(label="Open", command=lambda: print('open'))
-        filemenu.add_command(label="Save", command=lambda: print('save'))
-        filemenu.add_command(label="Exit", command=lambda: print('exit'))
+        filemenu = tk.Menu(menubar, tearoff=0)
+        # filemenu.add_command(label="New", command=donothing)
+        filemenu.add_command(label="Open", command=donothing)
+        filemenu.add_command(label="Save", command=donothing)
+        filemenu.add_command(label="Save as...", command=donothing)
+        filemenu.add_command(label="Close", command=donothing)
 
+        filemenu.add_separator()
+
+        filemenu.add_command(label="Exit", command=root.quit)
         menubar.add_cascade(label="File", menu=filemenu)
+        editmenu = tk.Menu(menubar, tearoff=0)
+        editmenu.add_command(label="Undo", command=donothing)
+
+        editmenu.add_separator()
+
+        editmenu.add_command(label="Cut", command=donothing)
+        editmenu.add_command(label="Copy", command=donothing)
+        editmenu.add_command(label="Paste", command=donothing)
+        editmenu.add_command(label="Delete", command=donothing)
+        editmenu.add_command(label="Select All", command=donothing)
+
+        menubar.add_cascade(label="Edit", menu=editmenu)
+        helpmenu = tk.Menu(menubar, tearoff=0)
+        helpmenu.add_command(label="Help Index", command=donothing)
+        helpmenu.add_command(label="About...", command=donothing)
+        menubar.add_cascade(label="Help", menu=helpmenu)
 
         self.parent.config(menu=menubar)
 
--- test.py
+++ test.py
@@ -1,2 +1,11 @@
-from zmain_interfaces import execute
-execute()
+from PyPDF2 import PdfFileMerger
+
+pdfs = ['file1.pdf', 'file2.pdf', 'file3.pdf', 'file4.pdf'][:2]
+
+merger = PdfFileMerger()
+
+for pdf in pdfs:
+    merger.append(pdf)
+
+merger.write("CreioEmTi_merged.pdf")
+merger.close()
